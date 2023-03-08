7c7aeb22f46f9192b06075ad9449f1ae47b03da1
MohamedSelimMaazouz993 <mohamedselim.maazouz@gmail.com>
2023-03-08 02:58:43 +0100

    TP SOAP

--- SQLiteRecettes/web/WEB-INF/classes/recettes.sql
+++ SQLiteRecettes/web/WEB-INF/classes/recettes.sql
@@ -1,17 +1,10 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "Ingredient" (
-	"idIngredient"	INTEGER,
-	"Nom"	TEXT,
-	PRIMARY KEY("idIngredient")
-);
-CREATE TABLE IF NOT EXISTS "RecetteIngredient" (
-	"idRecette"	NUMERIC,
-	"idIngredient"	NUMERIC
-);
 CREATE TABLE IF NOT EXISTS "Recettes" (
 	"idRecette"	INTEGER,
 	"Titre"	TEXT,
 	"Description"	TEXT,
 	PRIMARY KEY("idRecette")
 );
+INSERT INTO "Recettes" ("idRecette","Titre","Description") VALUES (1,'Poulet Basquaise','Poulet cuit avec poivrons/tomates ,  servi avec du riz'),
+ (2,'Poulet au Curry','Poulet Indien cuit au four avec sauce crémeuse et servie avec du riz basmati');
 COMMIT;
